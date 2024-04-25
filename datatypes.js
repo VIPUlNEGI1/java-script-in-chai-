ac087231febb5f94c7080db8cb062141c4063150
VIPUlNEGI1 <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-04-25 12:29:13 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //this is used for give instruction our compiler treate our code in newer verison
+
+// alert("vipul")
+// alert(2=1)
+// its is not used in nodejs compiler because this sentax is used only browser 
+
+console.log ("vipul")
