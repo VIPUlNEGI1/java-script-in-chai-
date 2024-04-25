f71da8130ca35012df28fd2079a0db9e0edbf7ef
VIPUlNEGI1 <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-04-25 12:29:30 +0000

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -4,4 +4,30 @@
 // alert(2=1)
 // its is not used in nodejs compiler because this sentax is used only browser 
 
-console.log ("vipul")
+let name ="vipul"//this is string datatype
+let age = 20 //this is num value
+let vipulgirlfriend =false// boolean  it is used for true / false
+
+// number range is 2 to 53
+// bigint  => it is used  to difine big number values because num is only store 0 to53
+// for example
+BigInt=34544433333
+
+
+//  null is the standalone value 
+let tem;
+
+
+// symbol? this is used for identifacation and give unick value any companent
+
+
+console.log(typeof "vipul")
+
+console.log( typeof null)
+// null type is object this is very spacil thing in js  and important
+
+
+
+
+
+
